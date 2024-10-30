a7672f6abb5cf6e74cec51f41aa4cbb2ad1f62f5
eriktumosaryan <erik.saryan@tumo.org>
2024-10-30 12:31:30 +0400

    Added main files

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/home')
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5040, debug=True)
